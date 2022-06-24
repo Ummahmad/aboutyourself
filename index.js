b7999381c03de7390bdf458beaabcadfb3c241bd
Ummahmad <oluwafunmisaidat01@gmail.com>
2022-06-24 16:01:48 +0100

    second commit

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-let myName = "Abdulrasheed Saidat";
-console.log(myName);
+let myName = "Name: Abdulrasheed Saidat";
+/*console.log(myName);*/
 
-let myHeight = "5.5fts"
-console.log(myHeight);
+let myHeight = "Height: 5.5fts"
+/*console.log(myHeight);*/
 
-let myCountry = "Nigeria"
-console.log(myCountry);
+let myCountry = "Country: Nigeria"
+/*console.log(myCountry);*/
 
 
-document.write("Name:" + " " + myName + "<br>");
+/*document.write("Name:" + " " + myName + "<br>");
 document.write("Height:" + myHeight + "<br>");
-document.write("Country:" + myCountry);
+document.write("Country:" + myCountry);*/
+
+alert(myName + "\n" + myHeight + "\n" + myCountry);
