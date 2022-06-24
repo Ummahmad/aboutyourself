eed8af18f4a165c24c00fa1bbb57d4145c104f0a
Ummahmad <oluwafunmisaidat01@gmail.com>
2022-06-24 14:25:36 +0100

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 let myName = "Abdulrasheed Saidat";
 console.log(myName);
 
-let myHeight = "5.5ftss"
+let myHeight = "5.5fts"
 console.log(myHeight);
 
 let myCountry = "Nigeria"
 console.log(myCountry);
 
 
-document.write(myName + "<br>");
-document.write(myHeight + "<br>");
-document.write(myCountry);
+document.write("Name:" + " " + myName + "<br>");
+document.write("Height:" + myHeight + "<br>");
+document.write("Country:" + myCountry);
