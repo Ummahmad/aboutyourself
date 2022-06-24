1ab5bf26d8ad258f6b847e0ef124bcccd08103ec
Ummahmad <oluwafunmisaidat01@gmail.com>
2022-06-24 10:52:37 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let myName = "Abdulrasheed Saidat";
+console.log(myName);
+
+let myHeight = "5.5ftss"
+console.log(myHeight);
+
+let myCountry = "Nigeria"
+console.log(myCountry);
+
+
+document.write(myName + "<br>");
+document.write(myHeight + "<br>");
+document.write(myCountry);
